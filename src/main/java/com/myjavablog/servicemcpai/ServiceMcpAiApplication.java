7a8b26382708a5384df3ea0c2010d8c3d885754b
Anup Bhagwat <anup.bhagwat7@gmail.com>
2026-05-02 19:37:31 +0530

    feat-categorize - Initial project structure

---
+++ src/main/java/com/myjavablog/servicemcpai/ServiceMcpAiApplication.java
@@ -0,0 +1,13 @@
+package com.myjavablog.servicemcpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMcpAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMcpAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myjavablog/servicemcpai/ServiceMcpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjavablog.servicemcpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMcpAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
